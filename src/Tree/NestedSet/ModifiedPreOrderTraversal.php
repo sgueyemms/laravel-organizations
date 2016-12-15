1049e2f7473ec8b2536742d3c0f7372e05a7181b
Saidou Gueye <sgueye@mmseducation.com>
2016-12-15 10:59:15 -0500

    Fix the handling of the counter

--- src/Tree/NestedSet/ModifiedPreOrderTraversal.php
+++ src/Tree/NestedSet/ModifiedPreOrderTraversal.php
@@ -11,17 +11,37 @@ namespace Mms\Organizations\Tree\NestedSet;
 
 class ModifiedPreOrderTraversal
 {
-    public function run(NodeInterface $node, $level = 0, $counter = 1)
+    /**
+     * @var int
+     */
+    private $counter = 1;
+
+    /**
+     * @param NodeInterface $node
+     * @param $level
+     * @return NodeInterface
+     */
+    private function doRun(NodeInterface $node, $level)
     {
-        $node->setLevel($level)->setLeftValue($counter++);
+        $node->setLevel($level)->setLeftValue($this->counter++);
 
         foreach ($node->getChildren() as $child) {
             /**
              * @var NodeInterface $child
              */
-            $this->run($child, $level+1, $counter++);
+            $this->doRun($child, $level+1);
         }
-        $node->setRightValue($counter+1);
+        $node->setRightValue($this->counter++);
         return $node;
+    }
+
+    /**
+     * @param NodeInterface $node
+     * @return NodeInterface
+     */
+    public function run(NodeInterface $node)
+    {
+        $this->counter = 1;
+        return $this->doRun($node, 0);
     }
 }
